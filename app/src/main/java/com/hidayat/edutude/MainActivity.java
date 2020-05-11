961f7fc2bba00501d1155808bb05def0d6af19c7
Muhammad Syafiul Umam <msyafiulumam@student.ub.ac.id>
2020-05-11 08:36:04 +0700

    bisa play video playlist youtube terpisah, tolong ditata lagi penempatan layoutnya.

--- app/src/main/java/com/hidayat/edutude/MainActivity.java
+++ app/src/main/java/com/hidayat/edutude/MainActivity.java
@@ -8,13 +8,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YouTubeBaseActivity {
+    private static final String TAG = "MainActivity";
 
 private FirebaseAuth mFirebaseAuth;
 private TextView mTvEmail;
+
+YouTubePlayerView  mYouTubePlayerView, nYoutubePlayerView;
+Button btn_play1, btn_play2;
+YouTubePlayer.OnInitializedListener mOnInitializedListener, nOnInitializedListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,53 @@ private TextView mTvEmail;
 
         mTvEmail = findViewById(R.id.tv_email);
         mFirebaseAuth = FirebaseAuth.getInstance();
+        btn_play1 = (Button) findViewById(R.id.btn_play);
+        mYouTubePlayerView = (YouTubePlayerView) findViewById(R.id.view_YouTubePlay);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadPlaylist("PLYVR0A4acpNYSCN-0f7f9TB8XgHKNmRhz");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        btn_play1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYouTubePlayerView.initialize(YoutubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
+
+
+        //===============
+        btn_play2 = (Button) findViewById(R.id.btn_play2);
+        nYoutubePlayerView = (YouTubePlayerView) findViewById(R.id.view_YouTubePlay2);
+
+        nOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadPlaylist("PLFt_AvWsXl0f4c56CbvYi038zmCmoZ4CQ");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        btn_play2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nYoutubePlayerView.initialize(YoutubeConfig.getApiKey(), nOnInitializedListener);
+            }
+        });
+        //===============
+
     }
 
     @Override
---
+++ app/src/main/java/com/hidayat/edutude/YoutubeConfig.java
@@ -0,0 +1,10 @@
+package com.hidayat.edutude;
+
+public class YoutubeConfig {
+    public YoutubeConfig(){
+    }
+    private static final String API_KEY = " AIzaSyDqe0qi-VXr7wv5naLwgibew5l2gEMt6po";
+    public static String getApiKey(){
+        return API_KEY;
+    }
+}
